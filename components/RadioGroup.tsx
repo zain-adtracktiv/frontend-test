15df3011366d22a945c504985f87715919555344
web-dexprt <web@dexprt.com>
2024-07-03 03:41:18 +0530

    few changes in reusability

--- components/RadioGroup.tsx
+++ components/RadioGroup.tsx
@@ -1,8 +1,15 @@
+// RadioGroup.tsx
 "use client";
 import { useState } from "react";
+import Card from "./Card";
+
+const cardData = [
+  { id: "card1", title: "3 Sticks (-32%)", price: "$ 64.00/each", bestDeal: true },
+  { id: "card2", title: "2 Sticks (-22%)", price: "$ 71.00/each" },
+  { id: "card3", title: "1 Stick (-15%)", price: "$ 75.00/each" },
+];
 
 function RadioGroup() {
-  
   const [selectedCard, setSelectedCard] = useState<string | null>(null);
 
   const handleSelect = (card: string) => {
@@ -11,82 +18,17 @@ function RadioGroup() {
 
   return (
     <div className="grid grid-cols-3 gap-x-14">
-      {/* card 1 */}
-      <div
-        className={`relative px-14 py-10 border-4 rounded-3xl bg-gray-100 cursor-pointer ${
-          selectedCard === "card1" ? "border-indigo-600 border-4" : "border-gray-300"
-        }`}
-        onClick={() => handleSelect("card1")}
-      >
-        <div className={`absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-xl px-4 py-1 font-normal bg-indigo-700 ${
-          selectedCard === "card1" ? "bg-indigo-600 text-white" : "bg-gray-300 text-black "
-        }`}>
-          <h3 className="text-xl">BEST DEAL</h3>
-        </div>
-        <div className="flex items-start gap-x-4 text-2xl">
-          <div>
-            <input
-              className="form-radio h-6 w-6 text-indigo-700 scale-125 accent-indigo-700"
-              type="radio"
-              name="deal"
-              checked={selectedCard === "card1"}
-              
-            />
-          </div>
-          <div>
-            <h2 className="font-bold tracking-wider">3 Sticks (-32%)</h2>
-            <p className="font-normal">$ 64.00/each</p>
-          </div>
-        </div>
-      </div>
-      {/* card 2 */}
-      <div
-        className={`px-14 py-10 border-4 rounded-3xl bg-gray-100 cursor-pointer ${
-          selectedCard === "card2" ? "border-indigo-600 border-4" : "border-gray-300"
-        }`}
-        onClick={() => handleSelect("card2")}
-
-      >
-        <div className="flex items-start gap-x-4 text-2xl">
-          <div>
-            <input
-              className="form-radio h-6 w-6 text-indigo-700 scale-125 accent-indigo-700"
-              type="radio"
-              name="deal"
-              checked={selectedCard === "card2"}
-              onChange={() => handleSelect("card2")}
-            />
-          </div>
-          <div>
-            <h2 className="font-bold tracking-wider">2 Sticks (-22%)</h2>
-            <p className="font-normal">$ 71.00/each</p>
-          </div>
-        </div>
-      </div>
-      {/* card 3 */}
-      <div
-        className={`px-14 py-10 border-4 rounded-3xl bg-gray-100 cursor-pointer ${
-          selectedCard === "card3" ? "border-indigo-600 border-4" : "border-gray-300"
-        }`}
-        onClick={() => handleSelect("card3")}
-
-      >
-        <div className="flex items-start gap-x-4 text-2xl">
-          <div>
-            <input
-              className="form-radio h-6 w-6 text-indigo-700 scale-125 accent-indigo-700"
-              type="radio"
-              name="deal"
-              checked={selectedCard === "card3"}
-              onChange={() => handleSelect("card3")}
-            />
-          </div>
-          <div>
-            <h2 className="font-bold tracking-wider">1 Stick (-15%)</h2>
-            <p className="font-normal">$ 75.00/each</p>
-          </div>
-        </div>
-      </div>
+      {cardData.map(({ id, title, price, bestDeal }) => (
+        <Card
+          key={id}
+          card={id}
+          selectedCard={selectedCard}
+          handleSelect={handleSelect}
+          title={title}
+          price={price}
+          bestDeal={bestDeal}
+        />
+      ))}
     </div>
   );
 }

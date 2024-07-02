7b23a755f60c3da7f3f14b87efa6a3ff2bd853c5
pedropankaj <mail.guptaz@gmail.com>
2024-07-03 03:14:16 +0530

    stick option done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+import SticksSelection from "./components/StickSelection";
+
 export default function Home() {
+  const stickOptions = [
+    { id: 'three-sticks', label: '3 Sticks (-32%)', price: '$64.00/each', bestDeal: true },
+    { id: 'two-sticks', label: '2 Sticks (-22%)', price: '$71.00/each', bestDeal: false },
+    { id: 'one-stick', label: '1 Stick (-15%)', price: '$75.00', bestDeal: false },
+  ];
   return (
     <div className="text-xl font-bold">
       Create a RadioGroup component and use it here
+      <SticksSelection options={stickOptions} />
     </div>
   );
 }

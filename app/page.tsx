1d39b76943c17e48e2ea816d15ea2961b7014ca7
Adham Tarek <adhamtarek291@gmail.com>
2024-07-03 00:30:22 +0300

    radio-group-component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+import RadioGroup from "@/components/ui/radio-group";
+
+import * as React from "react";
+
 export default function Home() {
+  const options = [
+    { label: "3 Sticks (-32%)", value: "46.00", each: true, bestDeal: true },
+    { label: "2 Sticks (-32%)", value: "71.00", each: true },
+    { label: "1 Sticks (-32%)", value: "75.00" },
+  ];
+  const [selectedValue, setSelectedValue] = React.useState(options[0].value);
+
   return (
-    <div className="text-xl font-bold">
-      Create a RadioGroup component and use it here
+    <div className="flex flex-col py-10 items-center justify-center">
+      <h1 className="text-xl font-bold mb-10">
+        Create a RadioGroup component and use it here
+      </h1>
+      <RadioGroup
+        options={options}
+        name="example"
+        selectedValue={selectedValue}
+        onChange={setSelectedValue}
+      />
     </div>
   );
 }
---
+++ components/ui/radio-group.tsx
@@ -0,0 +1,69 @@
+// components/RadioGroup.tsx
+import React from "react";
+
+interface RadioOption {
+  value: string;
+  label: string;
+  bestDeal?: boolean;
+  each?: boolean;
+}
+
+interface RadioGroupProps {
+  options: RadioOption[];
+  name: string;
+  selectedValue: string;
+  onChange: (value: string) => void;
+}
+
+const RadioGroup: React.FC<RadioGroupProps> = ({
+  options,
+  name,
+  selectedValue,
+  onChange,
+}) => {
+  return (
+    <div className="flex gap-4 flex-wrap">
+      {options.map((option) => (
+        <label
+          key={option.value}
+          className={`flex relative cursor-pointer gap-2 border-2 rounded-xl p-4 py-5 pr-6 ${
+            selectedValue === option.value
+              ? "bg-[#ECEBFE] border-[#4745EE]"
+              : "bg-[#DEE2EA] border-[#B7BED0]"
+          }`}
+        >
+          <input
+            type="radio"
+            name={name}
+            value={option.value}
+            checked={selectedValue === option.value}
+            onChange={() => onChange(option.value)}
+            className="hidden"
+          />
+          <span
+            className={`size-6 bg-white rounded-full ${
+              selectedValue === option.value
+                ? "border-[#5A58F2] border-[7px]"
+                : "border-[#B7BED0] border"
+            }`}
+          ></span>
+          <div className="flex flex-col">
+            <span className="text-[#14181F] font-bold text-xl">
+              {option.label}
+            </span>
+            <span className="text-[#262A32] text-sm">
+              {option.value}${option.each && "/each"}
+            </span>
+          </div>
+          {option.bestDeal && (
+            <div className="absolute text-white text-sm uppercase bg-[#5A58F2] rounded-xl p-3.5 py-1 w-fit -top-4 mx-auto left-0 right-0">
+              Best Deal
+            </div>
+          )}
+        </label>
+      ))}
+    </div>
+  );
+};
+
+export default RadioGroup;

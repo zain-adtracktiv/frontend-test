6ea8c3496c207af8d1adb1798a796138aa518d6c
ali khan <ali.khan@vrtechsol.com>
2024-07-04 20:14:55 +1000

    radio button task

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import RadioGroup from "@/components/RadioGroup";
+
 export default function Home() {
   return (
-    <div className="text-xl font-bold">
-      Create a RadioGroup component and use it here
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-2xl font-bold mb-4 text-center mt-[10px]">
+          Select Your Option
+        </h1>
+        <RadioGroup />
+      </header>
     </div>
   );
 }

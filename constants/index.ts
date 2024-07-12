ab05b240703972ac65d888a3eeb30e96800b777e
1986webdeveloper <1986webdeveloper@gmail.com>
2024-07-12 18:15:40 +0530

    Performed the test task

---
+++ constants/index.ts
@@ -0,0 +1,20 @@
+export const Offers = [
+  {
+    id: "offer1",
+    title: "3 Sticks (-32%)",
+    price: 64.0,
+    priceText: "each",
+  },
+  {
+    id: "offer2",
+    title: "2 Sticks (-22%)",
+    price: 71.0,
+    priceText: "each",
+  },
+  {
+    id: "offer3",
+    title: "1 Stick (-15%)",
+    price: 75.0,
+    priceText: "",
+  },
+];
